39d35f6e479e6bed40c5f4787a50503cbf2e25ed
jayakumar <mj@cal4care.com>
2021-05-11 07:53:54 +0530

    changes by MJ

--- src/app/home/appointments/appointments.component.ts
+++ src/app/home/appointments/appointments.component.ts
@@ -15,11 +15,15 @@ selectdate ;
 shownoimg =false;
 show_date;
 count;
-  constructor(private serverService: ServerService, private router:Router,private route: ActivatedRoute) { }
+today;
+  constructor(private serverService: ServerService, private router:Router,private route: ActivatedRoute) {
+
+  
+   }
 
   ngOnInit(): void {
-  	this.getappointments()
-
+this.selectdate=moment(new Date()).format('DD-MM-YYYY')
+this.getappointments()
   	var cur_date = new Date();
      this.formatshow_date(moment(cur_date).format('YYYY-MM-DD'),"initial");
 
@@ -27,6 +31,7 @@ count;
 
   getappointments(){
   this.shownoimg = false;
+
     let api_req:any = '{"slot_date":"'+this.selectdate+'"}';
     let url="appointments"
   
@@ -44,9 +49,12 @@ for (var i = 0; i < this.appointmentlist.length; i++) {
 this.appointmentlist[i].sl_color ="#f89406";
 this.appointmentlist[i].sl_color_bg ="#f4b85e3d";
 
+
 	}else{
 this.appointmentlist[i].sl_color ="#131ae296";
 this.appointmentlist[i].sl_color_bg ="#eeeff6";
+this.appointmentlist[i].sl_color_bg ="#f4b85e3d";
+
 
 	}
 }
@@ -82,15 +90,30 @@ var date;
 		console.log(e)
 	var splitdate = e.split('-')
  date = new Date(splitdate[0], splitdate[1]-1, splitdate[2]);  
+ if(moment(new Date()).format('DD-MM-YYYY') == moment(e).format('DD-MM-YYYY'))
+   this.today = "Today"
+ else
+   this.today =""
 
 	}else{
 		var splitdate = e.target.value.split('-')
  date = new Date(splitdate[0], splitdate[1]-1, splitdate[2]);  
-
+ if(moment(new Date()).format('DD-MM-YYYY') == moment(e.target.value).format('DD-MM-YYYY'))
+   this.today = "Today"
+ else
+   this.today =""
 	}
 const month = date.toLocaleString('default', { month: 'short' });
 this.show_date =  splitdate[2] +' ' +month + ' '+splitdate[0]
 
 }
+
+
+onActivate(event) {
+
+         
+
+}
+    
 
 }
